4d434e6b8c9983a217eb32fafff10f138c089940
VsrStudio <117354752+RassyaYa@users.noreply.github.com>
2024-10-24 22:04:25 +0700

    Update GuildUI.php

--- src/GuildUI/GuildUI.php
+++ src/GuildUI/GuildUI.php
@@ -2,20 +2,31 @@
 
 namespace GuildUI;
 
-use pocketmine\form\CustomForm;
+use pocketmine\form\CustomForm; // Pastikan untuk mengimpor CustomForm
+use pocketmine\form\Form; // Impor Form jika diperlukan
 use pocketmine\player\Player;
 
 class GuildUI {
+
+    private Main $plugin; // Menyimpan instance plugin utama
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+    }
+
     public function createGuildUI(Player $player): void {
         $form = new CustomForm(function (Player $player, ?array $data) {
             if ($data !== null) {
-                $guildName = $data[0]; // Mengambil input dari pemain
-                $player->sendMessage("Guild '$guildName' telah dibuat!");
+                $guildName = $data[0];
+                $this->plugin->getGuilds()[$player->getName()] = $guildName; // Simpan guild yang baru dibuat
+                $player->sendMessage("Guild '$guildName' has been created!");
+            } else {
+                $player->sendMessage("Guild creation cancelled.");
             }
         });
 
-        $form->setTitle("Buat Guild");
-        $form->addInput("Masukkan Nama Guild:");
-        $player->sendForm($form);
+        $form->setTitle("Create Guild");
+        $form->addInput("Enter Guild Name:"); // Menambahkan input untuk nama guild
+        $player->sendForm($form); // Mengirimkan form kepada pemain
     }
 }
