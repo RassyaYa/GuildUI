21c8676fde13e7b5543652685e70173ca7e3300f
VsrStudio <117354752+RassyaYa@users.noreply.github.com>
2024-10-24 22:01:59 +0700

    Update Main.php

--- src/GuildUI/Main.php
+++ src/GuildUI/Main.php
@@ -3,41 +3,48 @@
 namespace GuildUI;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\Player; // Pastikan diimpor
+use pocketmine\player\Player;
+use pocketmine\form\Form; // Impor untuk form
+use pocketmine\form\CustomForm; // Impor untuk CustomForm
 
-class Main extends PluginBase implements Listener {
-    private array $guilds = []; // Pastikan ini digunakan di tempat lain
+class Main extends PluginBase {
 
-    private GuildUI $guildUI;
+    private array $guilds = []; // Menyimpan guild yang ada
 
     public function onEnable(): void {
-        $this->guildUI = new GuildUI($this);
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->info("Plugin GuildUI Diaktifkan");
+        $this->getLogger()->info("GuildUI plugin enabled!");
     }
 
-    public function onJoin(PlayerJoinEvent $event): void {
-        // Logika untuk pemain baru yang bergabung
+    public function onDisable(): void {
+        $this->getLogger()->info("GuildUI plugin disabled!");
     }
 
+    // Fungsi untuk membuat guild UI
+    public function createGuildUI(Player $player): void {
+        $form = new CustomForm(function (Player $player, ?array $data) {
+            if ($data !== null) {
+                $guildName = $data[0]; // Mengambil nama guild dari input
+                $this->guilds[$player->getName()] = $guildName; // Menyimpan guild untuk pemain
+                $player->sendMessage("Guild '$guildName' has been created!");
+            } else {
+                $player->sendMessage("Guild creation cancelled.");
+            }
+        });
+
+        $form->setTitle("Create Guild");
+        $form->addInput("Enter Guild Name:"); // Input untuk nama guild
+        $player->sendForm($form); // Mengirim form ke pemain
+    }
+
+    // Fungsi untuk menangani perintah
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if ($command->getName() === "guild") {
-            if (!$sender instanceof Player) {
-                $sender->sendMessage("Perintah ini hanya bisa digunakan dalam permainan.");
-                return true;
-            }
-
-            switch ($args[0] ?? '') {
-                case "create":
-                    $this->guildUI->createGuildUI($sender);
-                    break;
-                default:
-                    $sender->sendMessage("Penggunaan: /guild [create|list|achievements|feedback|tournament]");
-                    break;
+            if ($sender instanceof Player) {
+                $this->createGuildUI($sender); // Memanggil fungsi untuk membuat guild UI
+            } else {
+                $sender->sendMessage("This command can only be used in-game.");
             }
             return true;
         }
