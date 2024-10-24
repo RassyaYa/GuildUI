dc6bad6d3168ea868f4f2adcd743b254b55fd614
VsrStudio <117354752+RassyaYa@users.noreply.github.com>
2024-10-24 22:04:55 +0700

    Update Main.php

--- src/GuildUI/Main.php
+++ src/GuildUI/Main.php
@@ -6,7 +6,6 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use pocketmine\form\Form; // Impor untuk form
 use pocketmine\form\CustomForm; // Impor untuk CustomForm
 
 class Main extends PluginBase {
@@ -21,28 +20,19 @@ class Main extends PluginBase {
         $this->getLogger()->info("GuildUI plugin disabled!");
     }
 
-    // Fungsi untuk membuat guild UI
     public function createGuildUI(Player $player): void {
-        $form = new CustomForm(function (Player $player, ?array $data) {
-            if ($data !== null) {
-                $guildName = $data[0]; // Mengambil nama guild dari input
-                $this->guilds[$player->getName()] = $guildName; // Menyimpan guild untuk pemain
-                $player->sendMessage("Guild '$guildName' has been created!");
-            } else {
-                $player->sendMessage("Guild creation cancelled.");
-            }
-        });
-
-        $form->setTitle("Create Guild");
-        $form->addInput("Enter Guild Name:"); // Input untuk nama guild
-        $player->sendForm($form); // Mengirim form ke pemain
+        $ui = new GuildUI($this); // Menginisialisasi GuildUI
+        $ui->createGuildUI($player); // Memanggil fungsi untuk membuat UI guild
     }
 
-    // Fungsi untuk menangani perintah
+    public function getGuilds(): array {
+        return $this->guilds; // Getter untuk guilds
+    }
+
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if ($command->getName() === "guild") {
             if ($sender instanceof Player) {
-                $this->createGuildUI($sender); // Memanggil fungsi untuk membuat guild UI
+                $this->createGuildUI($sender);
             } else {
                 $sender->sendMessage("This command can only be used in-game.");
             }
